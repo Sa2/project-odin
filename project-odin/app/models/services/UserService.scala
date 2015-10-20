0ae5281626f8067a578789e064c4894520b0ece0
Sa2 <ar-ciel_ar-dor_harvestasya@live.jp>
2015-10-20 23:07:43 +0900

    delete デバッグコードを削除

--- project-odin/app/models/services/UserService.scala
+++ project-odin/app/models/services/UserService.scala
@@ -12,8 +12,6 @@ import viewmodels.UserViewModel
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-import play.api.Logger
-
 /**
  * Created by Sa2 on 15/09/21.
  */
@@ -28,21 +26,16 @@ class UserService @Inject()(val userRepository: UserRepositoryLike) extends User
   import UserService._
 
   def findByUserId(userId: String): Future[Option[UserViewModel]] = {
-    Logger.info(userId)
     userRepository.findByUserId(userId).map( userOpt =>
       userOpt.flatMap { user =>
-        Logger.info(user.userId)
         Some(new UserViewModel(user))
       }
     )
   }
 
   def authenticate(form: LoginForm): Future[Option[UserViewModel]] = {
-    Logger.info("pass1")
     userRepository.findByUserId(form.userId).map { userOpt =>
-      Logger.info("pass2")
       userOpt.flatMap { user =>
-        Logger.info("pass3")
         if (hashAndStretch(form.password, user.passwordSalt, STRETCH_LOOP_COUNT) == user.password)
           Some(new UserViewModel(user))
         else
@@ -60,9 +53,6 @@ object UserService {
     (1 to STRETCH_LOOP_COUNT).foreach(i =>
       hashed = DigestUtils.sha256Hex(hashed + plain + salt)
     )
-
-    Logger.info("通過")
-    Logger.info(hashed)
     hashed
   }
 
