372b6a7988e9fbc5bbf8386289ae958ee0d2bd2b
Sa2 <ar-ciel_ar-dor_harvestasya@live.jp>
2015-10-27 22:36:00 +0900

    ディレクトリに合わせてシェルスクリプトを修正

--- db-manager/sbt-slick-codegen/gen-table.sh
+++ db-manager/sbt-slick-codegen/gen-table.sh
@@ -5,6 +5,6 @@ echo checking and making models directory
 [ ! -e ../../project-odin/app/$dir ] && mkdir -p ../../project-odin/app/$dir
 
 
-cp target/scala-2.11/src_managed/main/models/dao/Tables.scala ../../project-odin/app/models/dao/
+cp target/scala-2.11/src_managed/main/models/dao/Tables.scala ../../application/project-odin/app/models/dao/
 
 echo Generated!
