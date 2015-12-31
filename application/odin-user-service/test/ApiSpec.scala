4e676b80e492b460ef87ba29d07b5a8de5ba254b
Sa2 <ar-ciel_ar-dor_harvestasya@live.jp>
2016-01-01 01:53:30 +0900

    add テストコードを追記、最新のAPIに追従したテストに修正

--- application/odin-user-service/test/ApiSpec.scala
+++ application/odin-user-service/test/ApiSpec.scala
@@ -1,6 +1,7 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
+import play.api.libs.json.{Json, JsValue}
 
 import play.api.test._
 import play.api.test.Helpers._
@@ -12,8 +13,10 @@ import play.api.test.Helpers._
 class ApiSpec extends Specification {
   "Api" should {
 
-    "Api call test /user/api/v1/alone/1" in new WithApplication {
-      val user = route(FakeRequest(GET, "/user/api/v1/alone/1")).get
+    val userName = "fakeuser"
+
+    "Api call test /user/api/v1/fetch/id/1" in new WithApplication {
+      val user = route(FakeRequest(GET, "/user/api/v1/fetch/id/1")).get
       val regexStr = ".*user1.*".r
 
       status(user) must equalTo(OK)
@@ -24,8 +27,8 @@ class ApiSpec extends Specification {
       }
     }
 
-    "Api call test /user/api/v1/list" in new WithApplication {
-      val users = route(FakeRequest(GET, "/user/api/v1/list")).get
+    "Api call test /user/api/fetch/list" in new WithApplication {
+      val users = route(FakeRequest(GET, "/user/api/v1/fetch/list")).get
       val regexStr = ".*users.*".r
 
       status(users) must equalTo(OK)
@@ -34,6 +37,21 @@ class ApiSpec extends Specification {
         case regexStr(_*) => true
         case _ => false
       }
+    }
+
+    "Api call test /user/api/v1/create" in new WithApplication {
+      val requestParam = Json.obj(
+        "id"            -> 5,
+        "userId"        -> userName,
+        "password"      -> "password",
+        "name"          -> "テストユーザー",
+        "roleId"        -> 1,
+        "isLock"        -> false
+      )
+
+      val request = FakeRequest(POST, "/user/api/v1/create").withJsonBody(requestParam)
+      val response = route(request).get
+      contentAsString(response) == "{\"result\":\"success\"}"
     }
   }
 }
