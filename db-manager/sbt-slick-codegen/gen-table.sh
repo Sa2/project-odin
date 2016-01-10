a8a01ed4062f883360251e08ad9f41862f20776f
Sa2 <ar-ciel_ar-dor_harvestasya@live.jp>
2016-01-10 23:53:49 +0900

    modified #49 dao作成スクリプトにauthサービスを追加

--- db-manager/sbt-slick-codegen/gen-table.sh
+++ db-manager/sbt-slick-codegen/gen-table.sh
@@ -4,9 +4,11 @@ dir=models/dao/;
 echo checking and making models directory
 [ ! -e ../../application/project-odin/app/$dir ] && mkdir -p ../../application/project-odin/app/$dir
 [ ! -e ../../application/odin-user-service/app/$dir ] && mkdir -p ../../application/odin-user-service//app/$dir
+[ ! -e ../../application/odin-auth-service/app/$dir ] && mkdir -p ../../application/odin-auth-service//app/$dir
 
 
 cp target/scala-2.11/src_managed/main/models/dao/Tables.scala ../../application/project-odin/app/models/dao/
 cp target/scala-2.11/src_managed/main/models/dao/Tables.scala ../../application/odin-user-service/app/models/dao/
+cp target/scala-2.11/src_managed/main/models/dao/Tables.scala ../../application/odin-auth-service/app/models/dao/
 
 echo Generated!
